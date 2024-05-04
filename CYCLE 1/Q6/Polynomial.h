22a95bdd1a00bd482147cca85f55111fd768f11e
anujithpp <anujithpp2912004@gmail.com>
2024-05-04 17:33:31 +0530

    updated some codes

--- CYCLE 1/Q6/Polynomial.h
+++ CYCLE 1/Q6/Polynomial.h
@@ -1,25 +1,32 @@
 #include <iostream>
+
 using namespace std;
 
 struct Term {
     int coefficient;
     int exponent;
-    Term* next;
+    Term *next;
+
     Term(int p_coefficient, int p_exponent) : coefficient(p_coefficient), exponent(p_exponent), next(nullptr) {}
 };
 
 class Polynomial {
 private:
-    Term* head;
+    Term *head;
 
 public:
     Polynomial();
+
     ~Polynomial();
+
     void addTerm(int p_coefficient, int p_exponent);
+
     void display();
-    Polynomial add(Polynomial& other);
-    Polynomial multiply(Polynomial& other);
+
+    Polynomial add(Polynomial &other);
+
+    Polynomial multiply(Polynomial &other);
+
 };
 
-Polynomial getPolynomial();
 
